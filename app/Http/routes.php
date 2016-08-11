ebd39fbf9b7a41a82c507ada3d6dba0a5c311040
Mario Celi <mceli@stackbuilders.com>
2016-08-11 15:16:16 -0500

    Remove vendor directory

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,4 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get(
     '/', ['as' => 'home', function () {
         return response()->view('index');
@@ -21,18 +9,3 @@ Route::post(
     '/token',
     ['uses' => 'TokenController@generate', 'as' => 'token-generate']
 );
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    //
-});
